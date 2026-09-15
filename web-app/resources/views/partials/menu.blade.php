b43102051a9777dbf75c1da223e2133ee8f51c60
nantapongpa-a11y <nantapong.pa@kkumail.com>
2026-09-16 00:10:00 +0700

    Update menu.blade.php

--- web-app/resources/views/partials/menu.blade.php
+++ web-app/resources/views/partials/menu.blade.php
@@ -2,9 +2,12 @@
     <a href="{{ route('home') }}">หน้าแรก</a> /
     <a href="{{ route('search.index') }}">ค้นหา</a> /
     <a href="{{ route('agency.create') }}">แจ้งของหาย</a> /
-    <a href="{{ route('agency.index') }}">หน่วยงาน</a> /
-    <a href="{{ route('admin.index') }}">แอดมิน</a> /
-    <a href="{{ route('admin.stats') }}">สถิติ</a> /
+    <!-- เมนูของ user เฉพาะ แสดงหลังเข้าสู่ระบบแล้วเท่านั้น -->
+    @auth
+        <a href="{{ route('agency.index') }}">หน่วยงาน</a> /
+        <a href="{{ route('admin.index') }}">แอดมิน</a> /
+        <a href="{{ route('admin.stats') }}">สถิติ</a> /
+    @endauth
     <a href="{{ route('login') }}">เข้าสู่ระบบ</a> /
     <a href="{{ route('profile.edit') }}">โปรไฟล์</a>
 </nav>
